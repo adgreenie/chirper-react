16e12a24b7d57442aa30053938a2ca9510287714
Adam Green <adgreenie@gmail.com>
2020-04-23 17:59:23 -0400

    imported validateUser in Login, fixing Feed

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,7 +16,7 @@ function Main() {
           <Route exact path="/" component={SignUp} />
           <Route path="/login" component={Login} />
           <Route path="/create" component={ChirpForm} />
-          {/* <Route path="/feed" component={Feed} /> */}
+          <Route path="/feed" component={Feed} />
           <Route
             path="/user/:user"
             render={routerProps => <Userpage {...routerProps} />}
