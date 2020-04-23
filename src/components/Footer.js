c891886a1d632ae4a1727db509d0e2f2aee89ad6
Adam Green <adgreenie@gmail.com>
2020-04-23 08:51:28 -0400

    fixed nav structure and created context

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,54 @@
-import React from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import React, { useState, useContext } from "react";
+import {
+  Navbar,
+  NavItem,
+  NavLink,
+  Button,
+  UncontrolledPopover,
+  PopoverBody,
+} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
-import '../App.css'
+import ChirpForm from "./ChirpForm";
+import { AppContext } from "../App";
+import "../App.css";
 
 function Footer() {
-    return (
-        <nav className="bottomNav">
-            <Navbar color="secondary" light expand="md"  >
-            <Link to="/create">
-                <NavbarBrand href="/create"><i className="fas fa-play"></i></NavbarBrand>
-            </Link>
-            <NavbarBrand href="/users"><i className="fas fa-users"></i></NavbarBrand>
-            <Link to="/signup">
-                <NavbarBrand href="/signup"><i className="fas fa-sign-in-alt"></i></NavbarBrand>
-            </Link>
-            </Navbar>
-        </nav>
-    )
+  const app = useContext(AppContext);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+
+  const toggle = () => setPopoverOpen(!popoverOpen);
+
+  return (
+    <footer className="bottomNav">
+      <Navbar color="secondary" light expand="md">
+        <Button id="Popover1" type="button">
+          <i className="fas fa-play">
+            <UncontrolledPopover
+              trigger="legacy"
+              placement="top"
+              isOpen={popoverOpen}
+              target="Popover1"
+              toggle={toggle}
+            >
+              <PopoverBody>
+                <ChirpForm />
+              </PopoverBody>
+            </UncontrolledPopover>
+          </i>
+        </Button>
+        <NavItem>
+          <NavLink href={`/user/${app.user}`}>
+            <i className="fas fa-users" />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/signup">
+            <i className="fas fa-sign-in-alt" />
+          </NavLink>
+        </NavItem>
+      </Navbar>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,31 @@
 import React from "react";
-import { Navbar, Button, Nav, NavbarBrand , Col, Form, Label, Input, FormGroup} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Feed from "./Feed";
 import SignUp from "./SignUp";
 import ChirpForm from "./ChirpForm";
-import Userpage from "./Userpage"
-import { Route, Link } from "react-router-dom";
-import '../App.css'
+import Userpage from "./Userpage";
+import Login from "./Login";
+import { Route, Switch } from "react-router-dom";
+import "../App.css";
 
 function Main() {
   return (
     <main>
       <div className="tablet">
-        <ChirpForm/>
+        <ChirpForm />
       </div>
-      <section>
-          <Route exact path="/feed" component={Feed} />
-          <Route exact path="/create" component={ChirpForm} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/userpage" component={Userpage} />
-      </section>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/feed" component={Feed} />
+        <Route path="/create" component={ChirpForm} />
+        <Route path="/signup" component={SignUp} />
+        <Route
+          path="/user/:user"
+          render={routerProps => <Userpage {...routerProps} />}
+        />
+      </Switch>
     </main>
-  )
+  );
 }
 
 export default Main;
